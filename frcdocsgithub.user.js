5512b1ef790bcc2a67fd345caaf84bdb672c480f
Vasista Vovveti <vasistavovveti@gmail.com>
2022-06-15 19:41:50 -0700

    Update frcdocsgithub.user.js

--- frcdocsgithub.user.js
+++ frcdocsgithub.user.js
@@ -3,64 +3,73 @@
 // @namespace   TheTripleV
 // @match       https://github.com/wpilibsuite/frc-docs/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      TheTripleV
 // @description Better frc-docs github
 // 
 // ==/UserScript==
 
-function htmlToElement(html) {
+let htmlToElement = (html) => {
   let d = document.createElement('div');
   d.innerHTML = html.trim();
   return d.firstChild;
 }
 
-const starPaths = window.location.pathname.substring(22).split("/");
-const isPull = starPaths[0] == "pull";
-const isPullFiles = starPaths[2] == "files";
-const pullNumber = starPaths[1];
-const rtdBuildURL = `https://frc-docs--${pullNumber}.org.readthedocs.build/en/${pullNumber}/`;
-const rtdLatestURL = `https://docs.wpilib.org/en/latest/`;
+let starPaths;
+let isPull;
+let isPullFiles;
+let pullNumber;
+let rtdBuildURL;
+let rtdLatestURL;
 
-if (isPull) {
+let injectRTDPageButton = () => {
   const text = "RTD Build";
-  new MutationObserver(() => {
-    let editAndCodeButtons = document.querySelector("#partial-discussion-header > div.gh-header-show > div > div");
+  let editAndCodeButtons = document.querySelector("#partial-discussion-header > div.gh-header-show > div > div");
 
-    if (editAndCodeButtons.children[0].text == text) {
-      return;
-    }
-    
-    editAndCodeButtons.innerHTML = `<a href="${rtdBuildURL}" class="btn btn-sm d-inline-block float-left float-none m-0 mr-md-0" style="margin-right:4px !important;">${text}</a>` + editAndCodeButtons.innerHTML;
-  }).observe(document, {childList: true, subtree: true});
+  if (editAndCodeButtons.children[0].text == text) {
+    return;
+  }
+  editAndCodeButtons.innerHTML = `<a href="${rtdBuildURL}" class="btn btn-sm d-inline-block float-left float-none m-0 mr-md-0" style="margin-right:4px !important;">${text}</a>` + editAndCodeButtons.innerHTML;
 }
 
-if (isPullFiles) {
+let injectRTDFileButtons = () => {
+  for (const header of document.getElementsByClassName("js-replace-file-header-review")) {
+    if (!header) continue;
 
-  new MutationObserver(() => {
-    
-    for (const header of document.getElementsByClassName("js-replace-file-header-review")) {
-      if (!header) continue;
-
-      if (header.innerHTML.includes("RTD")) {
-        continue;
-      }
-      
-      let filename = header.querySelector('[name="path"]').value;
-      // if (! (filename.split(".").pop()) == "rst") {
-      //   continue;
-      // }
-      let path = filename.split("/").slice(1).join("/").replace(/\.rst$/, '.html');
-      let filePageURL = rtdBuildURL + path;
-      let latestPageURL = rtdLatestURL + path;
-      let rtdEmbedButton = htmlToElement(`
-        <a href="${filePageURL}" class="btn btn-sm ml-2">
-          RTD
-        </a>
-      `);
-      
-      header.prepend(rtdEmbedButton);
+    if (header.innerHTML.includes("RTD")) {
+      continue;
     }
-  }).observe(document, {childList: true, subtree: true});
+
+    let filename = header.querySelector('[name="path"]').value;
+    let path = filename.split("/").slice(1).join("/").replace(/\.rst$/, '.html');
+    let filePageURL = rtdBuildURL + path;
+    let latestPageURL = rtdLatestURL + path;
+    let rtdEmbedButton = htmlToElement(`
+      <a href="${filePageURL}" class="btn btn-sm ml-2">
+        RTD
+      </a>
+    `);
+
+    header.prepend(rtdEmbedButton);
+  }
+}
+
+new MutationObserver(() => {
+  starPaths = window.location.pathname.substring(22).split("/");
+  isPull = starPaths[0] == "pull";
+  isPullFiles = starPaths[2] == "files";
+  pullNumber = starPaths[1];
+  rtdBuildURL = `https://frc-docs--${pullNumber}.org.readthedocs.build/en/${pullNumber}/`;
+  rtdLatestURL = `https://docs.wpilib.org/en/latest/`;
+
+  if (isPull) {
+    injectRTDPageButton();
+  }
   
-}
+  if (isPullFiles) {
+    injectRTDFileButtons();
+  }
+  
+}).observe(document, {childList: true, subtree: true});
+
+
